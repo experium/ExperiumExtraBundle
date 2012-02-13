cf204ac78b29560562c7dd30f65b75b888f27fa7
Alexey Shockov <shokov@experium.ru>
2012-02-13 19:15:32 +0400

    Missed bundle class

---
+++ ExperiumExtraBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Experium\ExtraBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ExperiumExtraBundle extends Bundle
+{
+
+}
